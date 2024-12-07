2c2de1c28e05051c3e650f47b150c6c7fb225707
reedwi <riandolo103@gmail.com>
2024-12-07 14:14:32 -0500

    day 5 part 2

--- 05/solution.py
+++ 05/solution.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from itertools import permutations
 
 from aoc_utils.open_data import open_dataset_lines as odl, open_dataset_unparsed as odu
 from aoc_utils.types import ImportOption
@@ -45,11 +46,71 @@ def part_1():
             middle_values += ordering[len(ordering) // 2]
 
     return middle_values
-                
-
+            
 
 def part_2():
-    pass
+    # Build adjacency list representation of the graph
+    graph = defaultdict(set)
+    nodes = set()
+    
+    # Parse rules into graph
+    for row in data:
+        if not row:
+            break
+        before, after = map(int, row.split('|'))
+        graph[before].add(after)
+        nodes.add(before)
+        nodes.add(after)
+    
+    def topological_sort(numbers):
+        # Create subgraph for just the numbers we care about
+        local_graph = defaultdict(set)
+        for n1 in numbers:
+            for n2 in graph[n1]:
+                if n2 in numbers:
+                    local_graph[n1].add(n2)
+        
+        in_degree = defaultdict(int)
+        for n in numbers:
+            for neighbor in local_graph[n]:
+                in_degree[neighbor] += 1
+        
+        queue = [n for n in numbers if in_degree[n] == 0]
+        result = []
+        
+        while queue:
+            node = queue.pop(0)
+            result.append(node)
+            
+            for neighbor in local_graph[node]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+        
+        return result if len(result) == len(numbers) else None
+    
+    middle_sum = 0
+    for row in data:
+        if '|' in row or not row:
+            continue
+        numbers = [int(n) for n in row.split(',')]
+        
+        current_valid = True
+        for i, n1 in enumerate(numbers):
+            for n2 in numbers[i+1:]:
+                if n2 in graph[n1]:
+                    current_valid = False
+                    break
+            if not current_valid:
+                break
+        print('here')
+        if not current_valid:
+            sorted_nums = topological_sort(numbers)
+            if sorted_nums:
+                middle_sum += sorted_nums[len(sorted_nums) // 2]
+    
+    return middle_sum
+
 
 
 print(part_1())
