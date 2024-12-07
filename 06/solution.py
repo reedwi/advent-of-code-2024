c12cbd21fe2133be3556375efafe7d7cb96d4b7e
reedwi <riandolo103@gmail.com>
2024-12-07 15:32:49 -0500

    day 6 part 1

--- 06/solution.py
+++ 06/solution.py
@@ -13,8 +13,48 @@ match option:
     case ImportOption.FULL_UNPARSED:
         data = odu('06/dataset.txt')
 
+def get_next_pos(current_pos, current_dir):
+    match current_dir:
+        case '^':
+            next_pos = (current_pos[0], current_pos[1] - 1)
+        case 'v':
+            next_pos = (current_pos[0], current_pos[1] + 1)
+        case '<':
+            next_pos = (current_pos[0] - 1, current_pos[1])
+        case '>':
+            next_pos = (current_pos[0] + 1, current_pos[1])
+    return next_pos
+
 def part_1():
-    pass
+    for i, row in enumerate(data):
+        start = row.find('^')
+        if start != -1:
+            starting_pos = (start, i)
+            break
+    
+    visited_positions = set([starting_pos])
+    current_pos = starting_pos
+    current_dir = '^'
+
+    next_dirs = {
+        '^': '>',
+        'v': '<',
+        '<': '^',
+        '>': 'v'
+    }
+
+    while True:
+        next_pos = get_next_pos(current_pos, current_dir)
+
+        if next_pos[1] < 0 or next_pos[1] >= len(data) or next_pos[0] < 0 or next_pos[0] >= len(data[next_pos[1]]):
+            break
+        while data[next_pos[1]][next_pos[0]] == '#':
+            current_dir = next_dirs[current_dir]
+            next_pos = get_next_pos(current_pos, current_dir)
+
+        visited_positions.add(next_pos)
+        current_pos = next_pos
+    return len(visited_positions)
 
 def part_2():
     pass
